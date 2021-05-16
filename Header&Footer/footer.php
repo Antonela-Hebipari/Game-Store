cdc7614faaaa5eb0d1e51878c85b6c0db08e6bd7
Dario12321 <darjo.serbo@fshnstudent.info>
2021-05-16 16:08:53 +0200

    Footer&Header

---
+++ Header&Footer/footer.php
@@ -0,0 +1,36 @@
+        <footer>
+        <div class="footer">
+              <div class="container">
+                  <div class="row">
+                      <div class="col-sm-6 col-md-3 item">
+                          <h3>Contact Us</h3>
+                          <ul>
+                              <li><a href="#"><i class="fas fa-map-marker-alt"></i>Lagja 6 , Rruga Filan, Tirane</a></li>
+                              <li><a href="#">+355679368513</a></li>
+                              <li><a href="#">Nela.the.boss@gmail.com</a></li>
+                          </ul>
+                      </div>
+                      <div class="col-sm-6 col-md-3 item">
+                          <h3>About</h3>
+                          <ul>
+                              <li><a href="#">Company</a></li>
+                              <li><a href="#">Team</a></li>
+                              <li><a href="#">Careers</a></li>
+                          </ul>
+                      </div>
+                      <div class="col-md-6 item text">
+                          <h3>Company Name</h3>
+                          <p>This company was website was never planned to happen but hey,
+                            look where we are now.But anyways Ima go sleep since it's already 3 AM
+                            and I only slept like 3 hours yesterday (:
+                          </p>
+                      </div>
+                      <div class="col item social"><a href="#"><i class="icon ion-social-facebook"></i></a><a href="#"><i class="icon ion-social-twitter"></i></a><a href="#"><i class="icon ion-social-snapchat"></i></a><a href="#"><i class="icon ion-social-instagram"></i></a></div>
+                  </div>
+                  <p class="copyright">Gamescape © 2021</p>
+              </div>
+              </div>
+            
+          </footer>
+        </body>
+</html>
