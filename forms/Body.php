b55ae871d871320f121b7b8e410e473debfcc6be
Antonela-Hebipari <antonela.hebipari@fshnstudent.info>
2021-05-17 15:44:46 +0200

    Strukturova kodin, lidha faqet

--- forms/Body.php
+++ forms/Body.php
@@ -1,20 +1,12 @@
-<html>
-<head>
-<title>Homepage</title>
-  
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" >
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<?php
+include_once 'Head.html'; 
+?>
 <!-- Carousel-->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
 <body>
-
-<?php
-include_once 'Head.html'; 
-?>
 
 
 <div class="slider">
@@ -184,5 +176,3 @@ include_once 'Head.html';
     </section>
     
     <?php include_once 'Footer.html' ; ?>
-    </body>
-    </html>
