9fd306f9c359cb78b79eff19f13c5164d9774f9b
Antonela-Hebipari <antonela.hebipari@fshnstudent.info>
2021-05-25 20:56:48 +0200

    Shtova getAllProducts dhe getProductById logic

---
+++ get/getall.php
@@ -0,0 +1,54 @@
+<?php 
+//Headers
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+
+include_once '../config/database.php';
+include_once '../models/getproducts.php';
+
+//instantiate db and connect
+$database= new database();
+$db= $database->connect();
+
+// instantiate blog post object
+$products= new Products($db);
+
+//blog products query
+if($products->getAllProducts()===true){
+    echo json_encode(array(
+        'message'=> 'Error...'
+    ));
+}
+$result = $products->getAllProducts();
+
+//get row count
+$num= $result->rowCount();
+
+//check if any productss
+if($num>0){
+    //products array
+    $products_arr=array();
+    $products_arr['data']=array();
+
+    //i wanna fetch it as an associative array
+    while($row= $result-> fetch(PDO::FETCH_ASSOC)){
+        //extract cuz i wanna use $title, $body etc and not $row['title'] etc
+        extract($row);
+
+        $products_item=array(
+            'product_id'=> $product_id,
+            'product_name'=> $product_name,
+            'price'=> $price
+        );
+
+        //push to "data"
+        array_push($products_arr['data'], $products_item);
+    }
+    
+    //turn to json and output
+    echo json_encode($products_arr);
+} else {
+    // no productss
+    echo json_encode(array('message'=> 'No productss found.'));
+}
+?>
---
+++ models/getproducts.php
@@ -0,0 +1,79 @@
+<?php 
+class Products{
+    private $conn;
+    private $table='products';
+
+    public $category;
+    public $product_id;
+    
+    public $product_name;
+    public $description;
+    public $price;
+    
+    //if its a game
+    public $code_s;
+    public $ram;
+    public $graphics_card;
+    public $processor;
+    public $operating_system;
+
+    //if its an accessory
+    public $acc_type;
+    public $acc_type_name;
+
+    public function __construct($db){
+        $this->conn=$db;
+    }
+
+    public function getAllProducts(){
+        $result;
+        //create query 
+        $query='SELECT product_name, price, product_id FROM ' . $this->table;
+
+        //prepare statement 
+        $stmt= $this->conn->prepare($query);
+        
+        //execute query
+        if(!$stmt->execute()){
+            //true if sth went wrong
+            $result=true;
+            return result;
+        }
+        //otherwise return all products
+        else return $stmt;
+    }
+
+    public function getProductById(){
+        //create query 
+        $query='SELECT pr.product_name, pr.price, pr.description, pr.product_id, 
+        sf.ram, sf.graphics_card, sf.processor, sf.operating_system
+        FROM  products pr JOIN software_requirements sf ON sf.product_id=pr.product_id
+        WHERE pr.product_id=?;';
+        
+        //prepare statement 
+        $stmt= $this->conn->prepare($query);
+
+        //bind this id to the ? at the query
+        $stmt->bindParam(1,$this->product_id);
+
+        //execute query
+        if(!$stmt->execute()){
+            //true if sth went wrong
+            $result=true;
+            return result;
+        }
+
+        $row=$stmt->fetch(PDO::FETCH_ASSOC);
+        
+        //set properties
+        $this->product_name=$row['product_name'];
+        $this->price=$row['price'];
+        $this->description=$row['description'];
+        $this->ram=$row['ram'];
+        $this->graphics_card=$row['graphics_card'];
+        $this->processor=$row['processor'];
+        $this->operating_system=$row['operating_system'];
+        
+    }
+}
+?>
