afa779a7ffa0198f84104a7d39db94343ee05c6a
Dario12321 <84270452+Dario12321@users.noreply.github.com>
2021-06-02 12:05:22 +0200

    Add files via upload
    Faqja e produktit me style e vet

---
+++ forms/Product.php
@@ -0,0 +1,52 @@
+<?php
+
+readfile('Header.html');
+?>
+<head>
+<link rel="stylesheet" href="productstyle.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+<main class="container">
+ 
+  <!-- Left Column / Headphones Image -->
+  <div class="left-column" style="margin-bottom:150px;">
+    <img src="game13.jpg">
+    <div class="product-description"style="margin-top:10px;" >
+      
+      <span>System Requirements :</span>
+      <p class="RAM" style="margin-top:30px;">RAM :</p>
+      <p class="GraphicCard">Graphic Card :</p>
+      <p class="Processor">Processor :</p>
+      <p class="OperationSystem">Operation System</p>
+    </div>
+  </div>
+ 
+ 
+  <!-- Right Column -->
+  <div class="right-column">
+ 
+    <!-- Product Description -->
+    <div class="product-description">
+      <span>HORROR</span>
+      <h1>Little Nightmares II</h1>
+      <p>Little Nightmares II is a suspense-adventure game in which you play as Mono, a young boy trapped in a world that has been distorted by the humming transmission of a distant tower.With Six, the girl in a yellow raincoat, as his guide, Mono sets out to discover the dark secrets of The Signal Tower and save Six from her terrible fate.
+But their journey will not be straightforward as Mono and Six will face a gallery of new threats from the terrible residents of this world.</p>
+    </div>
+    <div class="product-description">
+      
+      <p class="ReleaseDate">Release Date :</p>
+      <p class="Quantity"> Quantity : </p>
+<!-- Product Pricing -->
+<div class="product-price">
+      <span>USD $29.99</span>
+      <a href="#" class="cart-btn">Add to cart</a>
+    </div>
+  </div>
+</div>
+</main>
+    
+</body>
+<?php readfile('Footer.html');?>
